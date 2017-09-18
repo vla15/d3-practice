745bd2caaa773b3bf15fe26e4b692a56a5e80382
Vincent La <vincela15@gmail.com>
2017-09-18 15:34:46 -0700

    adding d3 shape script

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var data = [100, 250, 175, 200, 120];
+var data = [100, 250, 175, 200, 120, 10];
+
+var extent = d3.extent(data, d => d);
+
+var svg = d3.select('svg');
+
+var yScale = d3.scaleLinear()
+  .domain(extent)
+  .range([300, 0] );
+
+console.log(yScale);
 
 
-d3.selectAll('rect')
+d3.select('svg')
+  .selectAll('rect')  
   .data(data)
+  .enter()
+  .append('rect')
   .attr('x', (d, i) => i * 100)
   .attr('y', d => 300 - d)
   .attr('width', 100)
