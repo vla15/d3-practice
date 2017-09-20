43afe73f00e80bb7f0ca9878884df18cb3b8ed9a
Vincent La <vincela15@gmail.com>
2017-09-20 12:41:22 -0700

    moving the .tsv file to the dist folder

--- server/index.js
+++ server/index.js
@@ -3,16 +3,9 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('index.html')
-})
 
 
-
-// app.use(express.static(path.join(__dirname, '../public')));
-
-
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => {
   console.log(`listening in on port: ${port}`);
