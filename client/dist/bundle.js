94ef97009660ee7e28a078e5aa2f50481c7d0b4b
Vincent La <vincela15@gmail.com>
2017-09-20 14:38:31 -0700

    commenting out the bar graph code

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -70,60 +70,41 @@
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_d3__ = __webpack_require__(170);
-// var data = [100, 250, 175, 200, 120, 10];
 
-// console.log(d3.tsvParse(weatherData));
+var data = [100, 250, 175, 200, 120, 10];
 var margin = {top: 20, left: 20, right: 20, bottom: 20};
 var height = 1000;
 var width = 1000;
 var city = 'San Francisco';
 
-// d3.tsv('data.tsv', (err, data) => {
+// d3.csv('Most-Recent-Cohorts-Scorecard-Elements.csv', (err, data) => {
 //   data.forEach(d => {
-//     d.date = d3.timeParse('%Y%m%d')(d.date);
-//     d.date = new Date(d.date);
-//     d[city] = +d[city]
+//     d.institution = d.INSTNM;
+//     d['GRAD_DEBT_MDN10YR_SUPP'] = isNaN(d['GRAD_DEBT_MDN10YR_SUPP']) ? 0 : +d['GRAD_DEBT_MDN10YR_SUPP'];
 //   })
-//   var yMax = d3.max(data, d => d[city])
+//   var yMax = d3.max(data, d => d['GRAD_DEBT_MDN10YR_SUPP'])
 //   var yScale = d3.scaleLinear()
 //     .domain([0, yMax])
 //     .range([height - margin.bottom, margin.top]);
 
-//   var xExtent = d3.extent(data, d => d.date);
-//   var xScale = d3.scaleTime()
-//     .domain(extent)
-//     .range([margin.left, width - margin.right]);
+//   var yAxis = d3.axisLeft()
+//     .scale(yScale);
 
-//   var svg = d3.select('svg');
+//   var svg = d3.select('svg')
 
-//   svg.append('rect')
+//   svg.append('g')
+//     .attr('transform', `translate(${margin.left}, ${margin.top})`)
+//     .call(yAxis);
+
+//   svg.selectAll('rect')
+//     .data(data)
+//     .enter()
+//     .append('rect')
 //     .attr('width', 2)
-//     .attr('height', d => yScale(d[city]))
-//     .attr('x', d => xScale(d.date))
-//     .attr('y', d => yScale(d[city]))
-//     .attr('fill', 'blue')
-// })
-
-// var svg = d3.select('svg');
-
-// var yScale = d3.scaleLinear()
-//   .domain(extent)
-//   .range([300, 0] );
-
-// console.log(yScale);
-
-
-// d3.select('svg')
-//   .selectAll('rect')  
-//   .data(data)
-//   .enter()
-//   .append('rect')
-//   .attr('x', (d, i) => i * 100)
-//   .attr('y', d => 300 - d)
-//   .attr('width', 100)
-//   .attr('height', d => d)
-//   .attr('fill', 'blue')
-//   .attr('stroke', '#fff');
+//     .attr('height', (d) => { return height - yScale(d['GRAD_DEBT_MDN10YR_SUPP']) })
+//     .attr('y', (d) => { return yScale(d['GRAD_DEBT_MDN10YR_SUPP']) })
+//     .attr('x', (d,i) => { return i + margin.left })
+//   });
 
 /***/ }),
 /* 1 */
